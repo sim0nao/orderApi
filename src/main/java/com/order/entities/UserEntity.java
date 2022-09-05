946501f3e97f7dad9cd3cce32c327e003e70f63b
simone campos <simone.campos@fatec.sp.gov.br>
2022-09-05 16:04:43 -0300

    Created h2 database

--- src/main/java/com/order/entities/UserEntity.java
+++ src/main/java/com/order/entities/UserEntity.java
@@ -3,9 +3,22 @@ package com.order.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
 public class UserEntity implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column (name= "user_id")
 	private Long id;
 	private String name;
 	private String email;
